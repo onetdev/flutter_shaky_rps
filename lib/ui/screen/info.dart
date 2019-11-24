0e94e8c5764ad897d4a1dd08d8015f7d346f6541
József Koller <orosznyet@gmail.com>
2019-11-24 22:05:24 +0100

    Add details to about page.

--- lib/ui/screen/info.dart
+++ lib/ui/screen/info.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shaky_rps/vars/shake_set.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Info extends StatefulWidget {
@@ -9,17 +10,17 @@ class Info extends StatefulWidget {
 }
 
 class _Info extends State<Info> {
+  static var headline = TextStyle(
+    color: const Color(0xfff23861),
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+  );
+
+  static var paragraph = TextStyle(color: Colors.white);
+
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
-
-    var headline = TextStyle(
-      color: const Color(0xfff23861),
-      fontSize: 25,
-      fontWeight: FontWeight.bold,
-    );
-
-    var paragraph = TextStyle(color: Colors.white);
 
     return Scaffold(
       appBar: AppBar(
@@ -39,8 +40,9 @@ class _Info extends State<Info> {
         color: const Color(0xff1f2b3e),
         width: deviceSize.width,
         height: deviceSize.height,
-        child: ListView(padding:  EdgeInsets.symmetric(horizontal: 15), children: [
-          Text('Disclaimer!', style: headline),
+        child:
+            ListView(padding: EdgeInsets.symmetric(horizontal: 15), children: [
+          Text('Disclaimer', style: headline),
           Text(
               "\nAlways hold your device tightly when shaking because if you drop it I'm are not responsible.\n",
               style: paragraph),
@@ -51,17 +53,49 @@ class _Info extends State<Info> {
           Text(
               "2.) Shake the phone and the result will be on your screen as soon as you stop shaking your phone.\n",
               style: paragraph),
-          Text('About', style: headline),
+          Text("Possible outcomes", style: headline),
+          SizedBox(height: 10),
+          _getRollTable(),
+          SizedBox(height: 20),
+          Text("Who did this?", style: headline),
           Text(
-            "\nI'm a Hungarian developer who loves Flutter and this was one of my experiments that ended up in the store. I hope you like it.\n" +
+            "\nI'm a Hungarian \ud83c\udded\ud83c\uddfa developer who loves Flutter and this was one of my experiments that I wanted to share. " +
+                "So, here it goes.\n" +
                 "\nIf you have any question, tap on the envelop in the right top corner.",
             style: paragraph,
           ),
-          Text(
-              "\nPs: I don't collect any data about you.\n",
-              style: paragraph),
         ]),
       ),
+    );
+  }
+
+  Column _getRollTable() {
+    var rows = List<Widget>();
+
+    ShakeGameSets.getModes().forEach((name, mode) {
+      rows.add(Text(
+        mode.icon.text,
+        style: TextStyle(
+          color: const Color(0xfff23861),
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+
+      var cols = List<Widget>();
+      mode.items.forEach((elem) {
+        cols.add(Padding(
+          padding: EdgeInsets.only(top: 7, bottom: 10, right: 10),
+          child: Icon(elem.icon, color: Colors.white),
+        ));
+      });
+
+      rows.add(Row(children: cols));
+    });
+
+    return Column(
+      children: rows,
+      crossAxisAlignment: CrossAxisAlignment.start,
     );
   }
 
